894bcb1871f5d63d82556af2523200770f380c83
Kyle Dawkins <kyle.dawkins@weareinstrument.com>
2013-04-23 11:37:32 -0700

    Some documentation changes

--- resources/public/js/caribou-docs.js
+++ resources/public/js/caribou-docs.js
@@ -4,6 +4,8 @@ $( function() {
   // class: brush for the wrong tag.
   $("code[class^=brush]").each(function(index, el) {
     $(el).parent().attr("class", $(el).attr("class"));
+    var html = $(el).html();
+    $(el).parent().empty().html(html);
   });
 
   SyntaxHighlighter.all();
