a7d61a5ce69cd9b8cbfb6ccc72298fbcfc3d57ca
ryan spangler <ryan.spangler@gmail.com>
2013-09-18 11:10:37 -0700

    updated to latest versions, documentation notice on README

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 # Caribou Documentation
 
+* This project no longer holds the Caribou documentation! *
+
+Access Caribou docs here: http://caribou.github.io/caribou/docs/outline.html
+
+## Documentation Wiki Engine
+
 This is a self-contained caribou application that acts as a documentation server.
 
 To start the server:
@@ -8,7 +14,7 @@ To start the server:
 
 and then navigate to
 
-    http://localhost:33333/
+    http://localhost:33333
 
 Its structure and format are tbd.  You can add/edit/delete pages directly in the wiki if you are also logged in to the admin backend:
 
--- project.clj
+++ project.clj
@@ -1,10 +1,12 @@
-(defproject caribou-docs "0.11.0"
+(defproject caribou/caribou-docs "0.12.0"
   :description "The page routing ring handler for caribou"
-  :dependencies [[org.clojure/clojure "1.4.0"]
-                 [antler/caribou-frontend "0.11.24"]
-                 [antler/caribou-admin "0.11.30"]
-                 [antler/caribou-api "0.11.20"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [caribou/caribou-frontend "0.12.12"]
+                 [caribou/caribou-admin "0.12.12"]
+                 [caribou/caribou-api "0.12.12"]
                  [markdown-clj "0.9.19"]]
+  :plugins [[caribou/lein-caribou "2.4.6"]
+            [lein-ring "0.8.6"]]
   :jvm-opts ["-agentlib:jdwp=transport=dt_socket,server=y,suspend=n"]
   :source-paths ["src" "../src"]
   :resource-paths ["resources/" "../resources/"]
